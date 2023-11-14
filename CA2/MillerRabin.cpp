0a0b28d70a6e1b8704b2a0fb4a1e67e2f364d4c4
James Clarke <jamesclarkedublin@gmail.com>
2023-11-14 22:12:02 +0000

    Code Refacters

--- CA2/MillerRabin.cpp
+++ CA2/MillerRabin.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 #include <math.h>
 
+int millerRabin(int n) {
+    int k, q, a;
 
-int main(void)
-{
-    int n, k, q, a;
-
-    n = 11;
-    k=0;
-    q=n-1;
+    k = 0;
+    q = n-1;
     
     //1. Finding k and q where k > 0, q is odd, so that: (n-1)==(2^k)*q)
     while (q % 2 == 0) {  //keep interating until q is not even
@@ -21,20 +18,33 @@ int main(void)
     
     //3. if (a^q) % n == 1 then return "inconclusive"
     if (int(pow(a,q)) % n == 1) {
-        std::cout << "Inconclusive\n";
-        return 1;    
+        return 0;    
     }
 
     //4. for j=0 to k-1 do
     for(int j=0; j<k; j++) {
         if(int(pow(a, (pow(2, j) * q))) % n == n-1) {  //5. if a^(2^j)*q mod n == n-1
-            std::cout << "Inconclusive";
-            return 1;
+            return 0;
         }  
     }
 
     //6. return "composite"
-    std::cout << "Composite";
+    return 1;
+}
 
-    return 0;
+
+int main(void)
+{
+    int input;
+
+    std::cout << "Please enter a number: ";
+    std::cin >> input;
+    
+    
+    if(millerRabin(input) == 0) {
+        std::cout << "Prime";
+    } else {
+        std::cout << "Composite";
+    }
+       
 }
